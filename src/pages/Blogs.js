eb1b256aaecc923e6c664ae1f206de4ad84c8a0f
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-22 22:35:43 +0000

    adding

---
+++ src/pages/Blogs.js
@@ -0,0 +1,6 @@
+const Blogs = () => {
+	return <h1 style={{textAlign: "center"}}>Tech news</h1>;
+};
+
+export default Blogs;
+
---
+++ src/pages/Contact.js
@@ -0,0 +1,23 @@
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import {DiGithubBadge} from 'react-icons/di'
+import {IconContext} from 'react-icons';
+import {BsYoutube} from 'react-icons/bs';
+import {BsLinkedin} from 'react-icons/bs';
+
+function Contact() {
+	return (
+		<>
+		 <Stack gap={4} className="col-md-5 mx-auto">
+		  <div></div>
+		  <Button variant="light" size="lg"><BsLinkedin/> LinkedIn</Button>
+		  <Button variant="light" size="lg"><DiGithubBadge/> Github</Button>
+		  <Button variant="light" size="lg"><BsYoutube/> Youtube</Button>
+		</Stack>
+		</>
+	);
+}
+
+export default Contact;
+
+
