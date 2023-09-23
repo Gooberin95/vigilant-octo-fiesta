b555c6313c2f273f87250c87006cf314adf59358
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-23 18:29:06 +0000

    building back up from reverted commit location

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Outlet, Link } from 'react-router-dom';
 
-function App() {
+function Layout() {
   return (
 	  <>
-	   <h1>hello world</h1>
+	   <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+	    <Container>
+	     <Navbar.Brand href="#home">Brandon Ruiz</Navbar.Brand>
+	      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+	      <Navbar.Collapse id="basic-navbar-nav">
+	      <Nav className="me-auto">
+	       <Link to="/">Home</Link>
+	       <Link to="/blogs">Blogs</Link>
+	       <Link to="/contact">Contact</Link>
+	  </Nav>
+	  </Navbar.Collapse>
+	  </Container>
+	  </Navbar>
+	    <Outlet />
+
 	  </>
   );
 }
 
-export default App;
+export default Layout;
