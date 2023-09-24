bc3dbc77e48d6ea1d11f54731db537118ab1a121
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-24 18:08:19 +0000

    added more href links to buttons

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,8 +9,8 @@ function Contact() {
 		<>
 		 <Stack gap={4} className="col-md-5 mx-auto">
 		  <div></div>
-		  <Button variant="light" size="lg"><BsLinkedin/> LinkedIn</Button>
-		  <Button variant="light" size="lg"><DiGithubBadge/> Github</Button>
+		  <Button href="https://www.linkedin.com/in/brandon-ruiz1/" variant="light" size="lg"><BsLinkedin/> LinkedIn</Button>
+		  <Button href="https://github.com/Gooberin95" variant="light" size="lg"><DiGithubBadge/> Github</Button>
 		  <Button href="https://www.youtube.com/channel/UC5y5JHcpvCzgcy04QcvLLBg" variant="light" size="lg"><BsYoutube/> Youtube</Button>
 		</Stack>
 		</>
