c50210ad46e36cf3db1a2bd7f10361b7ffe7f6a7
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-24 18:28:48 +0000

    adding home page correctly

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,7 +8,7 @@ function Layout() {
 		  	  <>
 		  	 <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
 		  	  <Container>
-		  	   <Navbar.Brand href="#home">Brandon Ruiz</Navbar.Brand>
+		  	   <Navbar.Brand href="/home">Brandon Ruiz</Navbar.Brand>
 		  	    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
 		  	    <Navbar.Collapse id="basic-navbar-nav">
 		  	    <Nav className="me-auto">
