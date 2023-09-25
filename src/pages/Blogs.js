0d44c884728d282edb536f05f30b4aa450e04c04
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-25 01:33:47 +0000

    saving progress

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -9,34 +11,24 @@ function Home() {
 
 
 		return (
-					<>
-						<Container>
-							<Row> 
-							 <Col><h1>>...Better Documentation</h1><p style={{fontSize: "1.5vw"}}>Documentation plays a pivotal role in numerous facets of life, from the world of software development to healthcare, education, and beyond. Its importance cannot be overstated for several compelling reasons.
-
-					Firstly, documentation serves as a vital knowledge repository. In fields like software development, clear and comprehensive documentation enables developers to understand code, troubleshoot issues, and collaborate effectively. It ensures that knowledge is not confined to individual team members but is readily accessible to anyone who needs it. Similarly, in healthcare, well-maintained patient records facilitate continuity of care, help medical professionals make informed decisions, and enhance patient safety.
-
-					Secondly, documentation fosters accountability and transparency. In organizational contexts, having documented processes, policies, and procedures ensures that responsibilities are clearly defined, reducing the risk of misunderstandings or disputes. In legal and regulatory domains, documentation is often a prerequisite for compliance, ensuring that organizations adhere to industry standards and government regulations. Moreover, it enhances transparency by allowing stakeholders to track and understand the rationale behind decisions and actions, promoting trust and credibility.
-
-					Lastly, documentation aids in learning and improvement. It serves as a valuable learning resource, helping individuals acquire new skills or knowledge. It allows for the analysis of past experiences, enabling organizations to identify areas for improvement and make informed decisions based on historical data. Whether in education, business, or personal development, documentation plays a crucial role in continuous growth and development.
-
-					In summary, documentation is a linchpin in various fields due to its capacity to store knowledge, promote accountability, transparency, and facilitate learning and improvement. Its significance extends far beyond mere record-keeping, as it underpins effective communication, collaboration, and decision-making in an increasingly complex and information-driven world.</p></Col>
-							 <Col><h3>>...Cloud World</h3><p>Vim, a highly versatile and powerful text editor, is renowned for its efficiency and utility in various computing environments. There are several key reasons why Vim is considered indispensable by many professionals and developers.
-
-					First and foremost, Vim's unique modal editing system sets it apart. It offers different modes for tasks like inserting text, navigating, and making edits, which might seem counterintuitive at first but ultimately leads to remarkable speed and precision. Users can move through files, manipulate text, and execute commands without lifting their hands from the keyboard, significantly enhancing productivity, especially for those who spend a substantial amount of time working with text.
-
-					Secondly, Vim's extensibility and customization capabilities are unmatched. It supports a wide array of plugins and scripting languages, allowing users to tailor their editing environment to suit their specific needs. This level of customization empowers individuals to create workflows and automate tasks, further streamlining their work and making Vim adaptable to different coding languages and development environments.
-
-					Lastly, Vim's ubiquity across various platforms and its lightweight nature make it an excellent choice for remote and server-based development. Whether working on a powerful desktop or a remote server via SSH, Vim remains consistent and functional, making it a reliable choice for developers and system administrators alike.
-
-					In summary, Vim's efficiency through modal editing, extensibility, and platform flexibility make it an invaluable tool for developers, writers, and anyone who works extensively with text. Its learning curve may be steep, but the mastery of Vim can lead to substantial productivity gains and a more efficient text-editing experience.</p>
-					</Col>
-
-							</Row>
-					</Container>
+			<> <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+			<Card className="text-center" bg="info" text="light" style={{ width: '18rem'}}>
+			 <Card.Header>Brandon Ruiz</Card.Header>
+			 <Card.Body>
+			   <Card.Title>Cloud and Code</Card.Title>
+			   <Card.Text>
+			    Welcome to my site, hosted on Azure!
+			  </Card.Text>
+			  <Button variant="primary">Click here!</Button>
+			 </Card.Body>
+			<Card.Footer className="text-muted">2 days ago</Card.Footer>
+		       </Card>
+		   </div>
 
 
-					</>
+			                   
+
+		       </>
 
 				)
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -6,8 +8,20 @@ function Home() {
 
 
 		return ( 
-		<>
-			Testing Home page..
+		<> <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+			<Card className="text-center" bg="dark" text="light" style={{ width: '48rem'}}>
+			 <Card.Header>Brandon Ruiz</Card.Header>
+			 <Card.Body>
+			   <Card.Title>Cloud and Code</Card.Title>
+			   <Card.Text>
+			    Welcome to my site, hosted on Azure!
+			  </Card.Text>
+			  <Button variant="primary">Click here!</Button>
+			 </Card.Body>
+			<Card.Footer className="text-muted">2 days ago</Card.Footer>
+		       </Card>
+		   </div>
+		);
 		</>
 
 				)
