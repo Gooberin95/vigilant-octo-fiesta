c9f438df038f929414f6ed79cd0151f431847656
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-26 18:39:56 +0000

    added api call with axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
 
 function Home() {
+	const [ quote, setQuote] = useState('')
+	
+	const getQuote = () => {
+		axios.get('https://api.quotable.io/random')
+		.then(res => {
+			console.log(res)
+			setQuote(res.data.content)
+		}).catch(err => {
+			console.log(err)
+		})
+	}
 
 
 		return ( 
@@ -15,8 +29,9 @@ function Home() {
 			   <Card.Title>Cloud and Code</Card.Title>
 			   <Card.Text>
 			    Welcome to my site, hosted on Azure!
+			{ quote && <p> {quote}</p>}
 			  </Card.Text>
-			  <Button variant="primary">Click here!</Button>
+			  <Button onClick={getQuote} variant="primary">Click here for a quote!</Button>
 			 </Card.Body>
 			<Card.Footer className="text-muted">2 days ago</Card.Footer>
 		       </Card>
