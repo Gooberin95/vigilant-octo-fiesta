8a22c29f19cd2568d4d01323d4647b7f7c952755
Brandon Ruiz <Sober4202020@outlook.com>
2023-09-26 20:18:52 +0000

    finding a layout for blog page

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -11,33 +13,25 @@ function Home() {
 
 		return (
 			<>
-			
-					      <Row xs={1} md={2} className="g-4">
-					        {Array.from({ length: 12 }).map((_, idx) => (
-							        <Col key={idx}>
-							          <Card>
-							            <Card.Body>
-							              <Card.Title>Card title</Card.Title>
-							              <Card.Text>
-							                This is a longer card with supporting text below as a natural
-							                lead-in to additional content. This content is a little bit
-							                longer.
-							              </Card.Text>
-							             <Button variant="primary">Open</Button>
-							            </Card.Body>
-							          </Card>
-							        </Col>
-							      ))}
-					      </Row>
-
-			 
-
-
-			                   
-
+			 <Container>
+			      <Row x style={{border: 'solid'}}s={2} md={4} lg={6}>
+			        <Col   style={{border: 'solid'}}>1 of 2</Col>
+			        <Col   style={{border: 'solid'}}>2 of 2</Col>
+			      </Row>
+			      <Row x style={{border: 'solid'}}s={1} md={2}>
+			        <Col  style={{border: 'solid'}}>1 of 3</Col>
+			        <Col   style={{border: 'solid'}}>2 of 3</Col>
+			        <Col  style={{border: 'solid'}}>3 of 3</Col>
+			      </Row>
+			      <Row  style={{border: 'solid'}}xs="auto">
+			        <Col  style={{border: 'solid'}}>1 of 3</Col>
+			        <Col  style={{border: 'solid'}}>2 of 3</Col>
+			        <Col  style={{border: 'solid'}}>3 of 3</Col>
+			      </Row>
+			    </Container>
 		       </>
 
- );
+		);
 };
 
 export default Home;
